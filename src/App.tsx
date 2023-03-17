5aea5171fda0e0115290f60267e5ababc2e07200
Jakov <j.jakovu@gmail.com>
2023-03-17 21:39:37 +0200

    Сортировка

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@ import PostForm from './Component/PostForm';
 import PostList from './Component/PostList';
 import MyButton from './Component/UI/button/MyButton';
 import MyInput from './Component/UI/input/MyInput';
+import MySelect from './Component/UI/select/MySelect';
 import './styles/App.css';
 
 function App() {
   const [posts, setPosts] = useState([
-    {id: 1, title: 'Javascript', body: 'Description'},
-    {id: 2, title: 'Javascript 1', body: 'Description'},
-    {id: 3, title: 'Javascript 2', body: 'Description'},
+    {id: 1, title: 'aa', body: 'ff'},
+    {id: 2, title: 'ss 1', body: 'gg'},
+    {id: 3, title: 'bb 2', body: 'hh'},
   ])
+
+  const [selectedSort, setSelectedSort] = useState('');
 
   const createPost = (newPost: any) => {
     setPosts([...posts, newPost]);
@@ -21,10 +24,27 @@ function App() {
     setPosts(posts.filter(p => p.id !== post.id));
   }
 
+  const sortPosts = (sort: string) => {
+    setSelectedSort(sort);
+    setPosts([...posts].sort((a: any, b: any) => a[sort].localeCompare(b[sort])))
+  }
+
   return(
     <div className="App">
       <PostForm create={createPost}/>
-      {/* Условная отрисовка */}
+      {/* Отделяем форму от выпадающего списка */}
+      <hr style={{margin: '15px 0'}} />
+      <div>
+        <MySelect
+          value={selectedSort}
+          onChange={sortPosts}
+          defaultValue="Сортировка"
+          options={[
+            {value: 'title', name: 'По названию'},
+            {value: 'body', name: 'По описанию'}
+          ]}
+        />
+      </div>
       {posts.length !== 0
         ? <PostList remove={removePost} posts={posts} title='Посты про JS' />
         : <h1 style={{textAlign: 'center'}}>Посты не найдены!!!</h1>
