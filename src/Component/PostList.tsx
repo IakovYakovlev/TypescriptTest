f3d9c82fd19fd2f69c90f6a07555a53dde7d4168
Jakov <j.jakovu@gmail.com>
2023-03-18 23:59:21 +0200

    анимация

--- src/Component/PostList.tsx
+++ src/Component/PostList.tsx
@@ -1,3 +1,6 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import {CSSTransition, TransitionGroup} from 'react-transition-group';
 import PostItem from './PostItem';
 
 const PostList = ({ posts, title, remove } : any) => {
@@ -15,10 +18,18 @@ const PostList = ({ posts, title, remove } : any) => {
       <h1 style={{textAlign: 'center'}}>
         {title}
       </h1>
+      <TransitionGroup>
+        {posts.map((post: any, index: number) => 
+            <CSSTransition
+            key={post.id}
+            timeout={500}
+            classNames="post"
+          >
+            <PostItem remove={remove} number={index + 1} post={post} /> 
+          </CSSTransition>
+        )}
+      </TransitionGroup>
 
-      {posts.map((post: any, index: number) => 
-        <PostItem remove={remove} number={index + 1} post={post} key={post.id} /> 
-      )}
     </div>
   );
 };
